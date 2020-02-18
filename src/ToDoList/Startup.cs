991d24b9a07087a96d31fbaa37c15bde37da4a79
Jānis Bērziņš <janis.berzins@visma.com>
2020-02-18 09:40:44 +0200

    Updated folders

--- src/ToDoList/Startup.cs
+++ src/ToDoList/Startup.cs
@@ -36,16 +36,8 @@ namespace DotNetCoreSqlDb
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
-            // Use SQL Database if in Azure, otherwise, use SQLite
-            if (Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Production")
-                services.AddDbContext<MyDatabaseContext>(options =>
-                        options.UseSqlServer(Configuration.GetConnectionString("dbConnectionString")));
-            else
-                services.AddDbContext<MyDatabaseContext>(options =>
-                        options.UseSqlite("Data Source=localdatabase.db"));
-
-            // Automatically perform database migration
-            services.BuildServiceProvider().GetService<MyDatabaseContext>().Database.Migrate();
+            services.AddDbContext<MyDatabaseContext>(options =>
+                    options.UseSqlite("Data Source=localdatabase.db"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
