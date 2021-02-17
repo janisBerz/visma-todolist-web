81978715bac90b6c501fe43a40429144d9d63665
janisBerz <janis.berzins@hmcts.net>
2021-02-18 00:51:01 +0200

    Adding script

---
+++ .scripts/efUpdate.ps1
@@ -0,0 +1,11 @@
+﻿# Delete old migrations
+rm -r Migrations
+# Recreate migrations
+dotnet ef migrations add InitialCreate
+
+# Set connection string to production database
+# PowerShell
+$env:ConnectionStrings:MyDbConnection="<connectionString>"
+
+# Run migrations
+dotnet ef database update
