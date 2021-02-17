1a176a47c4065dd4cc7ec72700fccc4693136849
janisBerz <janis.berzins@hmcts.net>
2021-02-17 02:06:50 +0200

    Removed database migratons

--- src/ToDoList/Startup.cs
+++ src/ToDoList/Startup.cs
@@ -38,10 +38,6 @@ namespace DotNetCoreSqlDb
                 services.AddDbContext<MyDatabaseContext>(options =>
                 options.UseSqlite("Data Source=localdatabase.db"));
             }
-
-            // Automatically perform database migration
-            //services.BuildServiceProvider().GetService<MyDatabaseContext>().Database.Migrate();
-
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
