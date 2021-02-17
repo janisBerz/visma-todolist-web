1861284d6e722fed6266fc790b743e4b6a88f758
janisBerz <janis.berzins@hmcts.net>
2021-02-17 03:07:06 +0200

    Updated startup

--- src/ToDoList/Startup.cs
+++ src/ToDoList/Startup.cs
@@ -26,18 +26,9 @@ namespace DotNetCoreSqlDb
             // The following line enables Application Insights telemetry collection.
             services.AddApplicationInsightsTelemetry();
 
-            // Use SQL Database if in Azure, otherwise, use SQLite
-            if (Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Production")
-            {
                 services.AddDbContext<MyDatabaseContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("dbConnectionString")));
-            }
-
-            else
-            {
-                services.AddDbContext<MyDatabaseContext>(options =>
-                options.UseSqlite("Data Source=localdatabase.db"));
-            }
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
